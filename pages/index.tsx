b412d739a45cced26f009592fead9c89d7c4de6a
Nate Look <nate@natelook.com>
2022-01-01 00:01:11 -0800

    add testnet

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ export default function HomePage() {
     <div className="text-white bg-black container mx-auto mt-10">
       <div className="flex items-center space-x-4">
         <h1 className="text-3xl">movr.domains</h1>
-        <span className="text-green mt-1.5">[Version 0.0.1]</span>
+        <span className="text-green mt-1.5">
+          [Version 0.0.1]{" "}
+          <span className="bg-red-500 text-white p-1 rounded text-xs ml-2 font-bold">
+            TESTNET
+          </span>
+        </span>
       </div>
       <div className="mt-3">
         <span className="text-red-500 uppercase font-bold text-sm">
