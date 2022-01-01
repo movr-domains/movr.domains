eb0b97116b0b89ddd2a04a2b84f30f526e0ee30d
Nate Look <nate@natelook.com>
2021-12-31 23:58:44 -0800

    setup tailwind; complete landingpage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#F2B705",
+        green: "#53CBC8",
+      },
+    },
+  },
+  plugins: [],
+};
