dd2d7e41aa11191348af74f9e63de321cc8e6bf6
Nate Look <nate@natelook.com>
2022-04-22 00:59:39 -0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'testnet.movr.domains', 'movr.domains'],
   },
   env: {
     NETWORK: process.env.NETWORK,
