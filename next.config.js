e98cd30f530e2951d41919cbd5c080e617c9384d
Nate Look <nate@natelook.com>
2022-04-30 20:19:13 -0700

    add domain as source

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['localhost', 'testnet.movr.domains', 'movr.domains'],
+    domains: [
+      'localhost',
+      'testnet.movr.domains',
+      'movr.domains',
+      'whispering-journey-12465.herokuapp.com',
+    ],
   },
   env: {
     NETWORK: process.env.NETWORK,
