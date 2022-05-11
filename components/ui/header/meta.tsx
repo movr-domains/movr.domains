20df4a96dcf59afb7562c4d4c2a2022a16ced143
Nate Look <nate@natelook.com>
2022-05-11 16:24:19 -0700

    add google analytics

--- components/ui/header/meta.tsx
+++ components/ui/header/meta.tsx
@@ -1,34 +1,53 @@
 import Head from 'next/head';
+import Script from 'next/script';
+import React from 'react';
 
 const Meta = () => (
-  <Head>
-    <link
-      rel='apple-touch-icon'
-      sizes='180x180'
-      href='/favicon/apple-touch-icon.png'
+  <React.Fragment>
+    <Head>
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/favicon/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/favicon/site.webmanifest' />
+      <link
+        rel='mask-icon'
+        href='/favicon/safari-pinned-tab.svg'
+        color='#5bbad5'
+      />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
+      <title>MOVR Domains</title>
+    </Head>
+    <Script
+      strategy='lazyOnload'
+      src={`https://www.googletagmanager.com/gtag/js?id=G-19TYPYZDT1`}
     />
-    <link
-      rel='icon'
-      type='image/png'
-      sizes='32x32'
-      href='/favicon/favicon-32x32.png'
-    />
-    <link
-      rel='icon'
-      type='image/png'
-      sizes='16x16'
-      href='/favicon/favicon-16x16.png'
-    />
-    <link rel='manifest' href='/favicon/site.webmanifest' />
-    <link
-      rel='mask-icon'
-      href='/favicon/safari-pinned-tab.svg'
-      color='#5bbad5'
-    />
-    <meta name='msapplication-TileColor' content='#da532c' />
-    <meta name='theme-color' content='#ffffff' />
-    <title>MOVR Domains</title>
-  </Head>
+
+    <Script id='google-analytics' strategy='lazyOnload'>
+      {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-19TYPYZDT1', {
+              page_path: window.location.pathname,
+            });
+                `}
+    </Script>
+  </React.Fragment>
 );
 
 export default Meta;
