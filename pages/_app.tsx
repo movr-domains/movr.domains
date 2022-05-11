d288804c394b383298a1449fb6cd86dd6a632a61
Nate Look <nate@natelook.com>
2022-05-11 16:25:53 -0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import "../styles/globals.css";
-import { Web3ReactProvider } from "@web3-react/core";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import { Web3ReactProvider } from '@web3-react/core';
+import type { AppProps } from 'next/app';
 import type {
   ExternalProvider,
   JsonRpcFetchFunc,
-} from "@ethersproject/providers";
-import { Web3Provider } from "@ethersproject/providers";
-import Head from "next/head";
+} from '@ethersproject/providers';
+import { Web3Provider } from '@ethersproject/providers';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
   const library = new Web3Provider(provider);
@@ -18,6 +19,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-19TYPYZDT1`}
+      />
+
+      <Script id='google-analytics' strategy='lazyOnload'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-19TYPYZDT1', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
     </Web3ReactProvider>
   );
 }
